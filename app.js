7175cc804f3cb2adb773c9bf32b20ea9c6c218fa
ItsMountain <mountaingame20@gmail.com>
2024-04-06 05:29:10 +0700

    bug on the date fixed

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const port = 3000;
 const path = require("path");
 const fetchRequest = require("./controllers/apiController.js");
 let currentTime = new Date();
-let tanggal = ''; // Initialize tanggal variable
 
 app.use(
   express.static(path.join(__dirname, "public"), {
@@ -21,87 +20,71 @@ app.use("/controllers", express.static(path.join(__dirname, "controllers")));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-function fetchDataAndUpdateTanggal() {
-  fetchRequest.then((waktu) => {
-    const jadwalData = waktu.data.jadwal;
-    tanggal = jadwalData.tanggal;
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
-}
+function fetchDataAndRenderPage() {
+  fetchRequest
+    .then((waktu) => {
+      const jadwalData = waktu.data.jadwal;
+      const tanggal = jadwalData.tanggal;
+      const waktuSubuh = jadwalData.subuh;
+      const waktuTerbit = jadwalData.terbit;
+      const waktuDzuhur = jadwalData.dzuhur;
+      const waktuAshar = jadwalData.ashar;
+      const waktuMaghrib = jadwalData.maghrib;
+      const waktuIsya = jadwalData.isya;
 
-fetchDataAndUpdateTanggal();
-
-setInterval(fetchDataAndUpdateTanggal, 24 * 60 * 60 * 1000);
-
-
-fetchRequest.then((waktu) => {
-    const jadwalData = waktu.data.jadwal;
-
-    // const tanggal = jadwalData.tanggal;
-
-    const waktuSubuh = jadwalData.subuh;
-    const waktuTerbit = jadwalData.terbit;
-    let waktuDzuhur = jadwalData.dzuhur;
-    const waktuAshar = jadwalData.ashar;
-    const waktuMaghrib = jadwalData.maghrib;
-    const waktuIsya = jadwalData.isya;
-
-    const dayOfWeek = new Date(tanggal).getDay();
-    if (dayOfWeek === 5) {
-        waktuDzuhur = "Jum'at";
-    }
-
-    // console.log(tanggal)
-
-    app.get("/", (req, res) => {
-      res.render("index", {
-        currentTime,
-        tanggal,
-        waktuSubuh,
-        waktuTerbit,
-        waktuDzuhur,
-        waktuAshar,
-        waktuMaghrib,
-        waktuIsya,
+      app.get("/", (req, res) => {
+        res.render("index", {
+          currentTime,
+          tanggal,
+          waktuSubuh,
+          waktuTerbit,
+          waktuDzuhur,
+          waktuAshar,
+          waktuMaghrib,
+          waktuIsya,
+        });
       });
-    });
 
-    app.get("/tv", (req, res) => {
-      res.render("tv", {
-        currentTime,
-        tanggal,
-        waktuSubuh,
-        waktuTerbit,
-        waktuDzuhur,
-        waktuAshar,
-        waktuMaghrib,
-        waktuIsya,
+      app.get("/tv", (req, res) => {
+        res.render("tv", {
+          currentTime,
+          tanggal,
+          waktuSubuh,
+          waktuTerbit,
+          waktuDzuhur,
+          waktuAshar,
+          waktuMaghrib,
+          waktuIsya,
+        });
       });
-    });
 
-    app.get('/fetchTanggal', (req, res) => {
-      // Fetch the tanggal data and send it as JSON
-      fetchRequest.then((waktu) => {
+      app.get('/fetchTanggal', (req, res) => {
+        fetchRequest.then((waktu) => {
           const jadwalData = waktu.data.jadwal;
           const tanggal = jadwalData.tanggal;
           res.json({ tanggal });
-      })
-      .catch((error) => {
+        }).catch((error) => {
           console.error("Error fetching tanggal data:", error);
           res.status(500).json({ error: 'Internal Server Error' });
+        });
       });
-  });  
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+
+      
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+// Initial fetch and render
+fetchDataAndRenderPage();
+
+// Periodically fetch data and render the page every day (86400000 milliseconds = 24 hours)
+setInterval(fetchDataAndRenderPage, 86400000);
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}/`);
 });
-
 
 // tests here
 
@@ -126,3 +109,4 @@ app.listen(port, () => {
 
 // console.log("Current time:", CurrentTime);
 // console.log("Time for tomorrow:", tomorrowTime);
+
